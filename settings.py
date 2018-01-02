b2c7eb3672549cdb1daac6f8a349681d78ac3e51
evandrocoan <evandrocoan@hotmail.com>
2018-01-02 16:47:39 -0200

    Moved the sublime_plugin.reload_plugin() example on settings.py to
    the top of the file, to ease the access to it.

--- settings.py
+++ settings.py
@@ -30,6 +30,11 @@
 import os
 import sublime
 
+# from channel_manager.channel_utilities import print_all_variables_for_debugging
+# print_all_variables_for_debugging( g_channelSettings )
+# import sublime_plugin
+# sublime_plugin.reload_plugin( "channel_manager.channel_utilities" )
+
 from channel_manager.channel_utilities import clean_urljoin
 from channel_manager.channel_utilities import run_channel_setup
 
@@ -102,7 +107,7 @@ def plugin_loaded():
     g_channelSettings['PACKAGES_TO_INSTALL_EXCLUSIVELY'] = \
     [
         "ActiveViewJumpBack",
-        "amxmodx",
+        "Amxmodx",
         "AmxxChannel",
         "AmxxPawn",
         "ChannelManager",
@@ -154,9 +159,4 @@ def plugin_loaded():
         """
 
     run_channel_setup( g_channelSettings, CURRENT_PACKAGE_NAME, CURRENT_PACKAGE_ROOT_DIRECTORY )
-
-    # from channel_manager.channel_utilities import print_all_variables_for_debugging
-    # print_all_variables_for_debugging( g_channelSettings )
-    # import sublime_plugin
-    # sublime_plugin.reload_plugin( "channel_manager.channel_utilities" )
 
